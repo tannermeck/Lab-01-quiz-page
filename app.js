4bb213a4eff824c77bc4130f22754ddf530295c0
TANNER MECK <steve.meck246@gmail.com>
2021-06-29 11:48:11 -0700

    answers logged to console

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const quizButton = document.getElementById('click-me');
 const results = document.getElementById('results');
 
 quizButton.addEventListener('click', ()=> {
-  console.log()
-})
+    const confirmed = confirm('Would you like to start the quiz');
+    console.log(confirmed);
+    if (confirmed){
+        const name = prompt('What is your name?');
+        const ans1 = prompt('Do I like sleeping?');
+        const ans2 = prompt('Do I like squirrels?');
+        const ans3 = prompt('Do I like to bark at the UPS guy?');
+        console.log(name, ans1, ans2, ans3);
+        
+    } else {
+        console.log('user cancelled');
+    }
+});
